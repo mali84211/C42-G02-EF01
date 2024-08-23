a682c0a7e0a5df48762aad17f8ae1be5bbd323d4
mali84211 <mali84211@gmail.com>
2024-08-23 19:42:53 +0300

    All Ass

---
+++ C42-G02-EF01/Instructor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C42_G02_EF01
+{
+    internal class Instructor
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public decimal Salary { get; set; }
+        public string Address { get; set; }
+        public decimal HourRateBonus { get; set; }
+
+        [ForeignKey("Department")]
+        public int Dept_ID { get; set; }
+    }
+}
---
+++ C42-G02-EF01/Topic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C42_G02_EF01
+{
+    internal class Topic
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
